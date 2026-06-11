ba643f26dfbd1400bd719ed2a485a8ef343864c4
Saqib <saqib@Zains-MacBook-Pro.local>
2026-06-12 02:52:53 +0500

    added date joined field

--- crud_operations/employee/models.py
+++ crud_operations/employee/models.py
@@ -7,3 +7,4 @@ class Employee(models.Model):
     emp_salary = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     emp_email = models.EmailField(max_length=100, blank=True, default='')
     emp_phone = models.CharField(max_length=15, blank=True, default='')
+    date_joined = models.DateField(auto_now_add=True)
